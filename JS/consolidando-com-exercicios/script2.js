ca0ee73484891883e5e752e4ae8d8ba6092a5fcc
Ctech DAWA <ctechdawa>
2024-07-03 20:31:52 -0300

    Update 03/07 pt2

---
+++ JS/consolidando-com-exercicios/script2.js
@@ -0,0 +1,15 @@
+/*
+  Crie um algoritmo que transforme as notas do sistema
+  numérico para sistema de notas em caracteres tipo A B C
+
+  * de 90 para cima -   A
+  * entre 80 - 89   -   B
+  * entre 70 - 79   -   C
+  * entre 60 - 69   -   D
+  * menor que 60    -   F
+*/
+
+// Faça primeiro a lógica
+// Depois passe para uma função
+
+
---
+++ JS/consolidando-com-exercicios/script4.js
@@ -0,0 +1,14 @@
+/*
+  ### Celsius em fahrenheit ou o contrário
+
+  Crie uma função que receba uma string em celsius ou
+  fahrenheit e faça a transformação de uma unidade para
+  outra
+
+  C = (F - 32) * 5/9
+
+  F = C * 9/5 + 32
+*/
+
+// transformDegree('50F')
+
